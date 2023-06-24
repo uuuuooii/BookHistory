5136a8628eea2a967b23e96a672afeee6210bd5e
neo@zillinks.com <keunhey0107@gmail.com>
2023-06-24 18:59:34 +0900

    fix: 500에러 확인중

--- src/common/api/url.ts
+++ src/common/api/url.ts
@@ -1,3 +1,3 @@
 // const BOOK_POST_DATA_URL = 'http://localhost:3000/api/posts';
-const BOOK_POST_DATA_URL = 'https://book-history.vercel.app/api/posts';
+const BOOK_POST_DATA_URL = '/api/posts';
 export default BOOK_POST_DATA_URL;
