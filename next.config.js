32d1995dd331ba1bb7e098ac38025d3b7d7a32a8
neo@zillinks.com <keunhey0107@gmail.com>
2023-06-26 11:40:06 +0900

    Update: image domain 추가

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {};
 module.exports = {
   nextConfig,
   images: {
-    domains: ['i.pcmag.com', '*', 'postfiles.pstatic.net'],
+    domains: [
+      'i.pcmag.com',
+      '*',
+      'postfiles.pstatic.net',
+      'cloud.google.com',
+      'contents.kyobobook.co.kr',
+    ],
   },
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
