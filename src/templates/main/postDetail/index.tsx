2a21c1c4a5b1eee96e103e587bee71a7bbf326c1
neo@zillinks.com <keunhey0107@gmail.com>
2023-07-04 22:31:32 +0900

    fix: query 받아오는 기능 대체 찾는 중

--- src/templates/main/postDetail/index.tsx
+++ src/templates/main/postDetail/index.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import React from 'react';
 
 const PostDetail = () => {
   return (
-    <div>index</div>
+    <main>
+      <section>
+        <article>asd</article>
+      </section>
+    </main>
   );
 };
 
