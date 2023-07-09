100645c8d17e747d8811aa9b185517dd62ce9a65
neo@zillinks.com <keunhey0107@gmail.com>
2023-07-09 19:25:04 +0900

    Update: 경로 수정

--- src/templates/main/visual/index.tsx
+++ src/templates/main/visual/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import * as S from './style';
+import { Wrapper } from './style';
 
 const Visual = () => {
   return (
-    <S.Wrapper>
+    <Wrapper>
       <S.TitleContent>
         <S.Title>독서 기록</S.Title>
         <S.Desc>완독한 책을 기록하는 공간입니다</S.Desc>
       </S.TitleContent>
-    </S.Wrapper>
+    </Wrapper>
   );
 };
 
