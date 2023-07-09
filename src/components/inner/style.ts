11dac906e5ef33afe6e3db371167077a416d8dc5
neo@zillinks.com <keunhey0107@gmail.com>
2023-07-09 19:29:26 +0900

    Style: 스타일 수정

--- src/components/inner/style.ts
+++ src/components/inner/style.ts
@@ -6,5 +6,9 @@ const Wrapper = styled.section`
   width: 100%;
   height: 100vh;
   padding: 0 50px;
+
+  @media screen and (max-width: 599px) {
+    padding: 0 10px;
+  }
 `;
 export default Wrapper;
