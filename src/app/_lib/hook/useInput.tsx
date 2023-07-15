296ad5f4f7b587aadccd34acc8f93b83c2bfa4b3
neo@zillinks.com <keunhey0107@gmail.com>
2023-07-16 00:12:02 +0900

    Delete: 필요 없는 코드 삭제

--- src/app/_lib/hook/useInput.tsx
+++ src/app/_lib/hook/useInput.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 const useInput = (defaultValue?: string) => {
   const [inputValue, setInputValue] = useState<string>(defaultValue ?? '');
--- src/app/_lib/provider/styleProvider.ts
+++ src/app/_lib/provider/styleProvider.ts
@@ -1,5 +1,6 @@
 'use client';
 
+// provider 수정
 import { Global } from '@emotion/react';
 
 export default Global;
