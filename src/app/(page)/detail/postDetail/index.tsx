b20b27e22e45886dea3356f107576dac99bf205d
neo@zillinks.com <keunhey0107@gmail.com>
2023-07-20 16:19:21 +0900

    Delete: 필요없는 코드 삭제

--- src/app/(page)/detail/postDetail/index.tsx
+++ src/app/(page)/detail/postDetail/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useSearchParams, usePathname } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import { getBookPostData } from '@/app/_app/api/creatorBookPost';
 import { PostDataProps } from '@/app/_app/api/dto';
