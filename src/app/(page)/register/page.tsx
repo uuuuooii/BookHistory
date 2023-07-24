01e014f959b2fd5ddcefef7c13c5c291bbc85af3
neo@zillinks.com <keunhey0107@gmail.com>
2023-07-24 18:14:16 +0900

    Update: 로그인 작업중

--- src/app/(page)/register/page.tsx
+++ src/app/(page)/register/page.tsx
@@ -1,31 +1,67 @@
 'use client';
 
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Inner from '@/app/_components/inner';
+import * as S from './style';
 
 const Register = () => {
+  const [isErr, setIsErr] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        })
+      });
+
+      res.status === 201 && router.push('/login?succes=Account has been created');
+    } catch (err) {
+      setIsErr(true);
+    }
+  };
+
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          placeholder="username"
-          required
-        />
-        <input
-          type="email"
-          placeholder="email"
-          required
-        />
-        <input
-          type="password"
-          placeholder="password"
-          required
-        />
-        <button type="button">Register</button>
-      </form>
-      <Link href="/login">Login with an existing account</Link>
-    </div>
+    <Inner>
+      <S.Wrapper>
+        <S.Form onSubmit={handleSubmit}>
+          <S.Input
+            type="text"
+            placeholder="username"
+            required
+          />
+          <S.Input
+            type="email"
+            placeholder="email"
+            required
+          />
+          <S.Input
+            type="password"
+            placeholder="password"
+            required
+          />
+          <S.Button type="button">Register</S.Button>
+          <S.Link>
+            {isErr && 'Something went wrong!'}
+            <Link href="/login">Login with an existing account</Link>
+          </S.Link>
+        </S.Form>
+      </S.Wrapper>
+    </Inner>
   );
 };
 
---
+++ src/app/api/auth/register/router.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import bcrypt from 'bcryptjs';
+import connect from '@/app/_app/api/mongoose/db';
+import user from '@/app/_app/api/mongoose/schema/user';
+
+const POST = async (request: NextRequest) => {
+  const { name, email, password } = await request.json();
+
+  await connect();
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  // eslint-disable-next-line new-cap
+  const newUser = new user({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    return new NextResponse('User has been created', {
+      status: 201,
+    });
+  } catch (err) {
+    return new NextResponse('err.meesage', {
+      status: 500,
+    });
+  }
+};
+
+export default POST;
