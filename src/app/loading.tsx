42dc2514bc7e0fc4c498f878bc55b865b5ad654b
neo@zillinks.com <keunhey0107@gmail.com>
2023-09-08 01:07:32 +0900

    Update: loading page 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div>
+      {/* <LoadingSkeleton /> */}
+      <p>loading...</p>
+    </div>
+  );
+}
