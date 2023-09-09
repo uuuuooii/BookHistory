4e1cbc2d8fbdc3cab3caaaa6761792b52a4d9ae2
neo@zillinks.com <keunhey0107@gmail.com>
2023-09-10 00:19:19 +0900

    Delete: 주석 제거

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
   return (
     <div>
       {/* <LoadingSkeleton /> */}
