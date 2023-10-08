d51aaab05ae77094d714ccb93c5fc662d43a99e4
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-08 15:11:30 +0900

    Update: 주석 추가

--- src/app/components/listElement/index.tsx
+++ src/app/components/listElement/index.tsx
@@ -34,6 +34,8 @@ const ListElement = ({
   };
 
   useEffect(() => {
+    // React cache function
+    // React 캐시 기능을 사용하면 함수의 반환 값을 기억할 수 있어 동일한 함수를 한 번만 실행하면서 여러 번 호출할 수 있습니다.
     const getPostData = cache(async () => {
       const res = await getBookPostData();
       setPostData(res.data.reverse());
