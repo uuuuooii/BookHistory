163b06d6114f32334f187a289e1f02ae1a3d9903
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-19 22:55:17 +0900

    Update: 안쓰는 import 지우기

--- src/app/components/listElement/item/index.tsx
+++ src/app/components/listElement/item/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { PostDataProps } from '@/app/lib/api/dto';
